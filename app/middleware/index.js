de20df7d8dd3bb38eaa85d1062e0ab1759562044
David Henriquez <David@Davids-MacBook-Pro.local>
2017-06-15 16:11:04 -0400

    Changed client to deployed URL

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -12,7 +12,7 @@ const loader = require('lib/wiring/loader');
 const corsPort = +('GA'.split('').reduce((p, c)=> p + c.charCodeAt(), ''));
 
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || `http://localhost:${corsPort}`,
+  origin: process.env.CLIENT_ORIGIN || `http://mangoceylon.com`,
 };
 
 const before = (app) => {
